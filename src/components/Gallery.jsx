6b150e2194d9f3fda47e98171fe00994900f93b0
arzurlopez <arzurlopez@gmail.com>
2025-12-02 10:19:57 +0100

    hacemos mapa con marcadores

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,16 +1,53 @@
-import ImageGallery from "react-image-gallery"
-import "react-image-gallery/styles/css/image-gallery.css"
 
+import ImageGallery from "react-image-gallery"
 import { useNavigate } from "react-router-dom";
+import "react-image-gallery/styles/css/image-gallery.css"
 import escapadas from "../assets/data/escapadas.json"
+console.log(escapadas)
+
+const images = [
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+        description: "Propuesta 1"
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+        description: "Propuesta 2"
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+        description: "Propuesta 3"
+    },
+];
+
+/* export default function Gallery(){
+    const navigate = useNavigate();
+}  */ 
+
+
 function reDirigir(e) {
-  navigate("/rutas")
+    window.location.href = "/rutas"
+
 }
+
 export default function Gallery() {
-  return (
-    <>
-      <div className="gallery">
-        <ImageGallery items={escapadas} autoPlay={true} thumbnailPosition="bottom" onClick={reDirigir} /></div>
-    </>
-  )
+    return (
+        <>
+        <div className="gallery">
+
+            <ImageGallery
+            className="gallery"
+                items={escapadas}
+                autoPlay={true}
+                showBullets={true}
+                thumbnailPosition="bottom"
+                onClick={reDirigir}
+               
+            />
+            </div>
+        </>
+    )
 }
